ad0be8963f450ecbdeffbef5a7b741e8639361e3
Florian Reuschel <florian.reuschel@ownbit.net>
2023-06-24 11:36:53 +0200

    fix: don't use semibold font when only regular has been loaded

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@ const logoUrl = useAbsoluteUrl('logo.svg')
         <NuxtLink to="/" class="flex items-center gap-[1.5ch]">
           <!-- prettier-ignore -->
           <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="448" height="512" class="w-auto h-[1.25em]"><path fill="currentColor" d="M231.536 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L60.113 273H436c6.627 0 12-5.373 12-12v-10c0-6.627-5.373-12-12-12H60.113L238.607 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L3.515 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg>
-          <span class="whitespace-nowrap font-semibold">to the editor</span>
+          <span class="whitespace-nowrap">to the editor</span>
         </NuxtLink>
         <ColorModeSwitch />
       </header>
